f0f33233981b76292e00087739deac5b3731f4b5
Himasha3 <mashigayathri7@gmail.com>
2024-10-11 14:38:59 +0530

    className

--- src/test/java/day1/Locators.java
+++ src/test/java/day1/Locators.java
@@ -1,7 +1,11 @@
 package day1;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Locators {
 
@@ -26,7 +30,13 @@ public class Locators {
 		//driver.findElement(By.linkText("Tablets")).click();
 		
 		//partialLinkText
-		driver.findElement(By.partialLinkText("Table")).click();
+		//driver.findElement(By.partialLinkText("Table")).click();
+		
+		//classname
+		List<WebElement> headerLinks=driver.findElements(By.className("list-inline-item"));
+		
+		System.out.println("total number of header links "+headerLinks.size());
+		
 		
 
 	}
