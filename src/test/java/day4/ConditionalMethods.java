d49b2ea27986a8e50e645ce9b4d1cc0b3f379e14
Himasha3 <mashigayathri7@gmail.com>
2024-10-20 13:04:49 +0530

    Conditional Methods - isDisplayed()

---
+++ src/test/java/day4/ConditionalMethods.java
@@ -0,0 +1,25 @@
+package day4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ConditionalMethods {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://demo.nopcommerce.com/register");
+		
+		driver.manage().window().maximize();
+		
+		//isDispalyed
+		
+		WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+		
+		System.out.println("Display status of logo :"+logo.isDisplayed());
+
+	}
+
+}
