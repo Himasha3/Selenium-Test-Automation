a7d89f42a5ffe63bacc528ca87110e2186b7c72a
Himasha3 <mashigayathri7@gmail.com>
2024-10-20 12:40:08 +0530

    WebDriver Methods - getWindowHandles

--- src/test/java/day4/WebDriverMethods.java
+++ src/test/java/day4/WebDriverMethods.java
@@ -1,10 +1,13 @@
 package day4;
+import java.util.Set;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverMethods {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		
 		WebDriver driver= new ChromeDriver();
@@ -12,23 +15,32 @@ public class WebDriverMethods {
         //get url
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		 
+		Thread.sleep(5000);
 		
 		
 		//getTitle
 		
-		System.out.println(driver.getTitle());
+		//System.out.println(driver.getTitle());
 		
 		//getCurrentUrl
 		
-		System.out.println(driver.getCurrentUrl());
+		//System.out.println(driver.getCurrentUrl());
 		
 		//getPageSource
-		System.out.println(driver.getPageSource());
+		//System.out.println(driver.getPageSource());
 		
 		//getWindowHnadle
-		String windowid = driver.getWindowHandle();
+		//String windowid = driver.getWindowHandle();
 		
-		System.out.println("Window ID :"+windowid);
+		//System.out.println("Window ID :"+windowid);
+		
+		//getWindowHandles
+		driver.findElement(By.linkText("OrangeHRM, Inc")).click();
+		
+		Set<String> windowids = driver.getWindowHandles();
+		
+		System.out.println(windowids);
+		
 		
 	}
 
